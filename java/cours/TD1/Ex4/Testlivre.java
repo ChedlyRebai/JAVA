7e48d206fe3d7206d2c804c582c411f35ee780e4
Chedly Rebai <107418294+ChedlyRebai@users.noreply.github.com>
2022-10-11 02:17:43 +0200

    add testlivre

---
+++ java/cours/TD1/Ex4/Testlivre.java
@@ -0,0 +1,8 @@
+package cours.TD1.Ex4;
+
+public class Testlivre {
+    public static void main(String[] args) {
+        Livre l1 = new Livre("titre", "auteur", 2, 4);
+        System.out.println(l1.testPrix());
+    }
+}
