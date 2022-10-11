aff0f65c011b277e8415aaf5271a4699c534909f
Chedly Rebai <107418294+ChedlyRebai@users.noreply.github.com>
2022-10-11 02:21:00 +0200

    add testPrix function

--- java/cours/TD1/Ex4/Testlivre.java
+++ java/cours/TD1/Ex4/Testlivre.java
@@ -3,6 +3,10 @@ package cours.TD1.Ex4;
 public class Testlivre {
     public static void main(String[] args) {
         Livre l1 = new Livre("titre", "auteur", 2, 4);
-        System.out.println(l1.testPrix());
+        if (l1.testPrix()) {
+            System.out.println("le prix est exist");
+        } else {
+            System.out.println("le prix n'existe pas");
+        }
     }
 }
